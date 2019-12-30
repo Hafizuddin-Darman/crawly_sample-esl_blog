78fa09c1bda92822410594530cd15a18cd148c53
Hafizuddin-Darman <46783310+Hafizuddin-Darman@users.noreply.github.com>
2019-12-30 03:30:03 -0500

    add spider reader

--- lib/esl_blog/spiders/esl.ex
+++ lib/esl_blog/spiders/esl.ex
@@ -14,7 +14,46 @@ defmodule Esl do
   end
 
   @impl Crawly.Spider
-  def parse_item(_response) do
-    %Crawly.ParsedItem{:items => [], :requests => []}
+  def parse_item(response) do
+    # Getting new urls to follow
+    urls =
+      response.body
+      |> Floki.find("a.more")
+      |> Floki.attribute("href")
+
+    # Convert URLs into requests
+    requests =
+      Enum.map(urls, fn url ->
+        url
+        |> build_absolute_url(response.request_url)
+        |> Crawly.Utils.request_from_url()
+      end)
+
+    # Extract item from a page, e.g.
+    # https://www.erlang-solutions.com/blog/introducing-telemetry.html
+    title =
+      response.body
+      |> Floki.find("article.blog_post h1:first-child")
+      |> Floki.text()
+
+    author =
+      response.body
+      |> Floki.find("article.blog_post p.subheading")
+      |> Floki.text(deep: false, sep: "")
+      |> String.trim_leading()
+      |> String.trim_trailing()
+
+    text = Floki.find(response.body, "article.blog_post") |> Floki.text()
+
+    %Crawly.ParsedItem{
+      :requests => requests,
+      :items => [
+        %{title: title, author: author, text: text, url: response.request_url}
+      ]
+    }
+  end
+
+  def build_absolute_url(url, request_url) do
+    URI.merge(request_url, url) |> to_string()
   end
 end
